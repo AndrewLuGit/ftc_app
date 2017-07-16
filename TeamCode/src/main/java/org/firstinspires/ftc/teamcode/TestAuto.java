bc049c0fa790d18244f4e6f5be90e3c260d2030d
garyding <garyding5678@gmail.com>
2017-07-16 15:41:50 -0700

    created placeholder for test autonomous program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by garyding on 7/16/17.
+ */
+
+public class TestAuto extends LinearOpMode{
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
