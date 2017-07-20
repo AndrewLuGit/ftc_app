9b3a3d152447d9164993fd61097348d664627c33
AndrewLuGit <andrew.yunxiang@gmail.com>
2017-07-19 21:09:43 -0700

    Added hardwaremap to testauto program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -2,14 +2,27 @@ package org.firstinspires.ftc.teamcode;
 
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by garyding on 7/16/17.
  */
 
 public class TestAuto extends LinearOpMode{
+    private DcMotor driveLeft;
+    private DcMotor driveRight;
+    private DcMotor armMotor;
+    private Servo clawLeft;
+    private Servo clawRight;
     @Override
     public void runOpMode() throws InterruptedException {
-
+        driveLeft = hardwareMap.dcMotor.get("driveLeft");
+        driveRight = hardwareMap.dcMotor.get("driveRight");
+        armMotor = hardwareMap.dcMotor.get("armMotor");
+        clawLeft = hardwareMap.servo.get("clawLeft");
+        clawRight = hardwareMap.servo.get("clawRight");
+        waitForStart();
+        //Todo: Make Autonomous part
     }
 }
