c780695a1161881322e30f17b71f79d3c09392f2
Andrew Lu <andrew,yunxiang@gmail.com>
2017-11-05 19:46:59 -0800

    Added teleop control of jewelHitter servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech_auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech_auto.java
@@ -15,12 +15,14 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcontroller.external.samples.SensorColor;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
@@ -37,14 +39,12 @@ public class mech_auto extends LinearOpMode {
     private DcMotor driverb;
     private CRServo grabber;
     private Servo jewelHitter;
-    //private LynxI2cColorRangeSensor colorRange;
+    private LynxI2cColorRangeSensor colorRange;
     private BNO055IMU imu;
     private Orientation angles;
     private double heading;
     private OpenGLMatrix lastLocation = null;
     private VuforiaLocalizer vuforia;
-    private Position robotPos;
-    private Velocity robotVel;
     @Override
     public void runOpMode() throws InterruptedException {
         drivelf = hardwareMap.dcMotor.get("drivelf");
@@ -55,7 +55,7 @@ public class mech_auto extends LinearOpMode {
         driverb.setDirection(DcMotor.Direction.REVERSE);
         grabber = hardwareMap.crservo.get("grabber");
         jewelHitter = hardwareMap.servo.get("jewelHitter");
-        //colorRange = (LynxI2cColorRangeSensor) hardwareMap.get("color");
+        colorRange = (LynxI2cColorRangeSensor) hardwareMap.get("color");
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
         parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
@@ -65,67 +65,106 @@ public class mech_auto extends LinearOpMode {
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
         imu = hardwareMap.get(BNO055IMU.class,"imu");
         imu.initialize(parameters);
-        imu.getPosition();
-        //robotPos.unit = DistanceUnit.CM;
         VuforiaLocalizer.Parameters paramters2 = new VuforiaLocalizer.Parameters();
         paramters2.vuforiaLicenseKey = "AVpbLJb/////AAAAGXZuk17KREdul0PqldXjI4ETC+yUOY/0Kn2QZcusavTR02WKxGvyI4E5oodS5Jta30WYJtnJuH7AhLaMe8grr9UC2U3qlnQkypIAZsR8xa38f669mVIo9wujvkZpHzvscPZGdZ2NaheUepxU/asMbuldnDOo3TjSYiiEbk1N3OkxdTeMa4W+BOyrO6sD8L7bcPfnFpmuOPRv0+NeEUL638AjNyi+GQeHYaSLsu6u4ONKtwF+axjjg0W+LRgp5T/5oWxexW3fgoMrkijzsJ0I5OuxSdCeZ3myJthxcyHwHqdhuxmWFvFOoYgJ4k6LdGNijymNWqMp97utjg8YXMAguMLJU2QkPJvZQqbkzIdjzzQk";
         paramters2.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         this.vuforia = ClassFactory.createVuforiaLocalizer(paramters2);
         VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
         VuforiaTrackable relicTemplate = relicTrackables.get(0);
-        //telemetry.addData("Distance",colorRange.getDistance(DistanceUnit.CM));
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 100);
+        jewelHitter.setPosition(0.34);
+        telemetry.addLine("Init Ready");
         telemetry.update();
-        jewelHitter.setPosition(1.0);
+        /* start of the code */
         waitForStart();
-        /*imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
-        robotPos = imu.getPosition();
-        double initPos = robotPos.y;
-        while (robotPos.x != initPos+10){
-            robotPos = imu.getPosition();
-            drivelf.setPower(0.5);
-            driverf.setPower(0.5);
-            drivelb.setPower(0.5);
-            driverb.setPower(0.5);
+        // Clear telemetry
+        telemetry.clear();
+        /* lower jewel hitter, wait until in position */
+        telemetry.addData("Servo Position",jewelHitter.getPosition());
+        telemetry.update();
+        sleep(1000);
+        jewelHitter.setPosition(0.85);
+        while (jewelHitter.getPosition()!=0.85) {
+            sleep(500);
+            telemetry.addData("Servo Position",jewelHitter.getPosition());
+            telemetry.update();
         }
-        drivelf.setPower(0);
-        driverf.setPower(0);
-        drivelb.setPower(0);
-        driverb.setPower(0);*/
-        //Jewel/read pictograph
-        jewelHitter.setPosition(0.25);
         sleep(500);
-        imudrive(0.5,-0.5,0.5,-0.5,-45);
-        sleep(500);
-        jewelHitter.setPosition(1.0);
-        sleep(500);
-        imudrive(-0.5,0.5,-0.5,0.5,45);
-        //drive to cryptobox
-        //score
-        idle();
+        /* Detect the color */
+        if (colorRange.blue()>colorRange.red()) {
+            imudrive(-0.5,0.5,-0.5,0.5,15);
+            jewelHitter.setPosition(0.34);
+            while (jewelHitter.getPosition()!=0.34) {
+                sleep(500);
+            }
+            imudrive(0.5,-0.5,0.5,-0.5,0);
+        } else if (colorRange.red()>colorRange.blue()) {
+            imudrive(0.5,-0.5,0.5,-0.5,-15);
+            jewelHitter.setPosition(0.34);
+            while (jewelHitter.getPosition()!=0.34) {
+                sleep(500);
+            }
+            imudrive(-0.5,0.5,-0.5,0.5,0);
+        } else {
+            jewelHitter.setPosition(0.34);
+            while (jewelHitter.getPosition()!=0.34) {
+                sleep(500);
+            }
+        }
+        /* scan the pictograph */
+        /*RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        while (vuMark==RelicRecoveryVuMark.UNKNOWN){
+            vuMark = RelicRecoveryVuMark.from(relicTemplate);
+            telemetry.addData("vuMark",RelicRecoveryVuMark.from(relicTemplate));
+            telemetry.update();
+        }
+        /*drive to cryptobox */
+        //drivetime(-0.5,-0.5,-0.5,-0.5,1000);
+        /*if (vuMark==RelicRecoveryVuMark.LEFT){
+
+        } else if (vuMark==RelicRecoveryVuMark.CENTER){
+
+        } else if (vuMark==RelicRecoveryVuMark.RIGHT) {
+
+        }*/
+        /*score*/
+        imu.stopAccelerationIntegration();
     }
     private void imudrive(double lfPower,double rfPower, double lbPower, double rbPower,double turnDegrees){
-        double turnFinal = heading+turnDegrees;
-        while (heading!=turnFinal){
-            angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-            heading = AngleUnit.DEGREES.normalize(AngleUnit.DEGREES.fromUnit(angles.angleUnit,angles.firstAngle));
-            drivelf.setPower(lfPower);
-            driverf.setPower(rfPower);
-            drivelb.setPower(lbPower);
-            driverb.setPower(rbPower);
-            telemetry.addData("Heading",heading);
-            telemetry.update();
+        if (heading>turnDegrees) {
+            while (heading>turnDegrees){
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                heading = AngleUnit.DEGREES.normalize(AngleUnit.DEGREES.fromUnit(angles.angleUnit,angles.firstAngle));
+                drivelf.setPower(lfPower);
+                driverf.setPower(rfPower);
+                drivelb.setPower(lbPower);
+                driverb.setPower(rbPower);
+                telemetry.addData("Heading",heading);
+                telemetry.update();
+            }
+        } else if (heading<turnDegrees) {
+            while (heading<turnDegrees){
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                heading = AngleUnit.DEGREES.normalize(AngleUnit.DEGREES.fromUnit(angles.angleUnit,angles.firstAngle));
+                drivelf.setPower(lfPower);
+                driverf.setPower(rfPower);
+                drivelb.setPower(lbPower);
+                driverb.setPower(rbPower);
+                telemetry.addData("Heading",heading);
+                telemetry.update();
+            }
         }
         drivelf.setPower(0.0);
         driverf.setPower(0.0);
         drivelb.setPower(0.0);
         driverb.setPower(0.0);
     }
-    private void drivetime(double lfPower,double rfPower, double lbPower, double rbPower,long time){
+    private void drivetime(double lfPower,double rfPower, double lbPower, double rbPower,long milliseconds){
         drivelf.setPower(lfPower);
         driverf.setPower(rfPower);
         drivelb.setPower(lbPower);
         driverb.setPower(rbPower);
-        sleep(time);
+        sleep(milliseconds);
         drivelf.setPower(0.0);
         driverf.setPower(0.0);
         drivelb.setPower(0.0);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech_teleop2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech_teleop2.java
@@ -1,15 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.hardware.bosch.NaiveAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
 /**
  * Teleop for Mechanum Drive and Relic Recovery
  */
 @TeleOp(name="Mechanum Teleop 2",group="mechanum")
-public class mech_teleop2 extends OpMode {
+public class mech_teleop2 extends LinearOpMode {
     /*Declares drive motors: lf = left front,lb = left back,etc.*/
     private DcMotor drivelf;
     private DcMotor driverf;
@@ -17,21 +26,32 @@ public class mech_teleop2 extends OpMode {
     private DcMotor driverb;
     private CRServo grabber;
     private DcMotor grabberMotor;
+    private Servo jewelHitter;
+    private double jhPos;
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
         drivelf = hardwareMap.dcMotor.get("drivelf");
         driverf = hardwareMap.dcMotor.get("driverf");
         drivelb = hardwareMap.dcMotor.get("drivelb");
         driverb = hardwareMap.dcMotor.get("driverb");
         grabber = hardwareMap.crservo.get("grabber");
         grabberMotor = hardwareMap.dcMotor.get("grabberMotor");
+        jewelHitter = hardwareMap.servo.get("jewelHitter");
         drivelf.setDirection(DcMotor.Direction.REVERSE);
         drivelb.setDirection(DcMotor.Direction.REVERSE);
-    }
-
-    @Override
-    public void loop() {
-        //Math wizardry
+        grabberMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        drivelf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driverf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        drivelb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driverb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        drivelf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driverf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        drivelb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driverb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        jewelHitter.setPosition(0.5);
+        waitForStart();
+        while (opModeIsActive()){
+            //Math wizardry
         double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
         double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
         double rightX = gamepad1.right_stick_x;
@@ -39,25 +59,32 @@ public class mech_teleop2 extends OpMode {
         final double v2 = r * Math.sin(robotAngle) - rightX;
         final double v3 = r * Math.sin(robotAngle) + rightX;
         final double v4 = r * Math.cos(robotAngle) - rightX;
-        drivelf.setPower(v1);
-        driverf.setPower(v2);
-        drivelb.setPower(v3);
-        driverb.setPower(v4);
-        if(gamepad1.left_bumper){
-            grabber.setPower(1.0);
-        } else if(gamepad1.left_trigger>=0.5){
-            grabber.setPower(-1.0);
-        } else{
-            grabber.setPower(-0.01);
+        drivelf.setPower(v2);
+        driverf.setPower(v1);
+        drivelb.setPower(v4);
+        driverb.setPower(v3);
+            if(gamepad1.left_bumper || gamepad2.left_bumper){
+                    grabber.setPower(1.0);
+            } else if(gamepad1.left_trigger>=0.5 || gamepad2.left_trigger>=0.5){
+                grabber.setPower(-1.0);
+            } else{
+                grabber.setPower(-0.01);
+            }
+            if (gamepad1.right_bumper || gamepad2.right_bumper) {
+                grabberMotor.setPower(-0.5);
+            } else if (gamepad1.right_trigger >= 0.5 || gamepad2.right_trigger>=0.5) {
+                grabberMotor.setPower(0.5);
+            } else {
+                grabberMotor.setPower(0.0);
+            }
+            jhPos = jewelHitter.getPosition();
+            if (gamepad1.a||gamepad2.a) {
+                jewelHitter.setPosition(jhPos+0.01);
+            } else if (gamepad1.b||gamepad2.b) {
+                jewelHitter.setPosition(jhPos-0.01);
+            }
+            telemetry.addData("jhPos",jewelHitter.getPosition());
+            telemetry.update();
         }
-        if (gamepad1.right_bumper) {
-            grabberMotor.setPower(-1.0);
-        } else if (gamepad1.right_trigger >= 0.5) {
-            grabberMotor.setPower(1.0);
-        } else {
-            grabberMotor.setPower(0.0);
-        }
-        telemetry.addData("grabberPower",grabber.getPower());
-        telemetry.update();
     }
 }
