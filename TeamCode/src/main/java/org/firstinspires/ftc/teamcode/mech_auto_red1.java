1c2755500221655ed1ebbbca3bcacffb2f5ea3aa
Andrew Lu <andrew,yunxiang@gmail.com>
2017-11-12 13:05:47 -0800

    modulize the code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech_auto_red1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech_auto_red1.java
@@ -44,6 +44,10 @@ public class mech_auto_red1 extends LinearOpMode {
     private Orientation angles;
     private OpenGLMatrix lastLocation = null;
     private VuforiaLocalizer vuforia;
+    private boolean myTeamRed = true;
+    private int balanceStonePosition = 1;
+    private int myPitLocation = 0;    /* 1 : lfts 0: center -1 : right */
+
     @Override
     public void runOpMode() throws InterruptedException {
         drivelf = hardwareMap.dcMotor.get("drivelf");
@@ -68,14 +72,13 @@ public class mech_auto_red1 extends LinearOpMode {
         paramters2.vuforiaLicenseKey = "AVpbLJb/////AAAAGXZuk17KREdul0PqldXjI4ETC+yUOY/0Kn2QZcusavTR02WKxGvyI4E5oodS5Jta30WYJtnJuH7AhLaMe8grr9UC2U3qlnQkypIAZsR8xa38f669mVIo9wujvkZpHzvscPZGdZ2NaheUepxU/asMbuldnDOo3TjSYiiEbk1N3OkxdTeMa4W+BOyrO6sD8L7bcPfnFpmuOPRv0+NeEUL638AjNyi+GQeHYaSLsu6u4ONKtwF+axjjg0W+LRgp5T/5oWxexW3fgoMrkijzsJ0I5OuxSdCeZ3myJthxcyHwHqdhuxmWFvFOoYgJ4k6LdGNijymNWqMp97utjg8YXMAguMLJU2QkPJvZQqbkzIdjzzQk";
         paramters2.cameraDirection = VuforiaLocalizer.CameraDirection.FRONT;
         this.vuforia = ClassFactory.createVuforiaLocalizer(paramters2);
-        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
-        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+
         jewelHitter.setPosition(0.05);
         telemetry.addLine("Init Ready");
         telemetry.update();
         /* start of the code */
         waitForStart();
-        relicTrackables.activate();
+
         imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
         angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         // Clear telemetry
@@ -83,83 +86,19 @@ public class mech_auto_red1 extends LinearOpMode {
         grabber.setPower(-0.1);
         /* lower jewel hitter, wait until in position */
         jewelHitter.setPosition(0.55);
-        while (jewelHitter.getPosition()!=0.55) {
-            sleep(500);
-            telemetry.addData("Servo Position",jewelHitter.getPosition());
-            telemetry.update();
-        }
-        sleep(500);
-        /* Detect the color */
-        if (colorRange.blue()>colorRange.red()) {
-            telemetry.addLine("Blue");
-            telemetry.update();
-            imudrive(-15,0.3);
-            sleep(500);
-            jewelHitter.setPosition(0.05);
-            while (jewelHitter.getPosition()!=0.05) {
-                sleep(500);
-            }
-            imudrive(15,0.3);
-        } else if (colorRange.red()>colorRange.blue()) {
-            telemetry.addLine("Red");
-            telemetry.update();
-            imudrive(15,0.3);
-            sleep(500);
-            jewelHitter.setPosition(0.05);
-            while (jewelHitter.getPosition()!=0.05) {
-                sleep(500);
-            }
-            imudrive(-15,0.3);
-        } else {
-            telemetry.addLine("Error");
-            telemetry.update();
-            jewelHitter.setPosition(0.05);
-            while (jewelHitter.getPosition()!=0.05) {
-                sleep(500);
-            }
-        }
-        /* scan the pictograph */
-        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
-        drivelf.setPower(0.2);
-        drivelb.setPower(0.2);
-        driverf.setPower(0.2);
-        driverb.setPower(0.2);
-        while (vuMark == RelicRecoveryVuMark.UNKNOWN) {
-            telemetry.addLine("Scanning");
-            telemetry.update();
-            vuMark = RelicRecoveryVuMark.from(relicTemplate);
-        }
-        drivelf.setPower(0);
-        drivelb.setPower(0);
-        driverf.setPower(0);
-        driverb.setPower(0);
-        vuMark = RelicRecoveryVuMark.from(relicTemplate);
-        /*drive to cryptobox */
-        drivetime(1.0,1.0,1.0,1.0,1000);
-        imudrive(90,0.5);
-        drivetime(-0.5,-0.5,-0.5,-0.5,1500);
-        if (vuMark==RelicRecoveryVuMark.LEFT){
-            telemetry.addLine("Left");
-            telemetry.update();
-            drivetime(0.5,0.5,0.5,0.5,2100);
-        } else if (vuMark==RelicRecoveryVuMark.CENTER){
-            telemetry.addLine("Center");
-            telemetry.update();
-            drivetime(0.5,0.5,0.5,0.5,1850);
-        } else if (vuMark==RelicRecoveryVuMark.RIGHT) {
-            telemetry.addLine("Right");
-            telemetry.update();
-            drivetime(0.5,0.5,0.5,0.5,1500);
-        }
+
+        kickOpponentJewel(myTeamRed);
+        adjustPostionForImageScan(balanceStonePosition);
+        /* get my pit location by scan the Vulmark */
+        updateMyPitLocation();
+        /* get to the right postion before unload Glyphs */
+        scorePositioning();
+        /* unloading */
+        scoreGlyphs();
         /*score*/
-        imudrive(-90,0.5);
-        drivetime(0.5,0.5,0.5,0.5,500);
-        grabber.setPower(0.5);
-        sleep(1000);
-        grabber.setPower(-0.01);
-        drivetime(-0.5,-0.5,-0.5,-0.5,300);
         imu.stopAccelerationIntegration();
     }
+
     private void imudrive(double turnDegrees,double k1){
         angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         double initDegrees = angles.firstAngle;
@@ -208,5 +147,115 @@ public class mech_auto_red1 extends LinearOpMode {
         driverf.setPower(0.0);
         drivelb.setPower(0.0);
         driverb.setPower(0.0);
+    }
+
+    private void kickLeft(boolean isLeft)
+    {
+        if (isLeft) {
+            telemetry.addLine("kick left");
+            imudrive(-15,0.3);
+            sleep(500);
+            imudrive(15,0.3);
+        } else {
+            telemetry.addLine("kick right");
+            imudrive(15,0.3);
+            sleep(500);
+            imudrive(-15,0.3);
+        }
+    }
+
+    private boolean redAtRight() {
+        if (colorRange.red()>colorRange.blue()) {
+            telemetry.addLine("red at right");
+            return true;
+        } else {
+            telemetry.addLine("red at left");
+            return false;
+        }
+    }
+
+    private void kickOpponentJewel(boolean teamRed) {
+         /* Detect the color */
+        jewelHitter.setPosition(0.05);
+        /* ? really need sleep */
+        sleep(500);
+        if (teamRed) {
+            telemetry.addLine("Team red");
+            /* kick blue */
+            if (redAtRight()) {
+                kickLeft(true);
+            } else {
+                kickLeft(false);
+            }
+        } else { /*kick red */
+            telemetry.addLine("Team blue");
+            if (redAtRight()) {
+                kickLeft(false);
+            } else {
+                kickLeft(true);
+            }
+        }
+        telemetry.update();
+    }
+
+    private void adjustPostionForImageScan(int bsPosition) {
+        drivetime(0.5,0.5,0.5,0.5,300);
+    }
+
+    private void updateMyPitLocation() {
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTrackables.activate();
+        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        telemetry.addLine("Scanning");
+        telemetry.update();
+
+        while (vuMark == RelicRecoveryVuMark.UNKNOWN) {
+            /* may need sleep */
+            vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        }
+        vuMark = RelicRecoveryVuMark.from(relicTemplate);
+
+        if (vuMark==RelicRecoveryVuMark.LEFT){
+            telemetry.addLine("Left");
+            myPitLocation = 1;
+        } else if (vuMark==RelicRecoveryVuMark.CENTER){
+            telemetry.addLine("Center");
+            myPitLocation = 0;
+        } else if (vuMark==RelicRecoveryVuMark.RIGHT) {
+            telemetry.addLine("Right");
+            myPitLocation = -1;
+        }
+        telemetry.update();
+    }
+
+    private void locateCryptoBoxCenter(int bsPosition) {
+
+         /*drive to cryptobox */
+        drivetime(1.0,1.0,1.0,1.0,1000);
+        imudrive(90,0.5);
+        drivetime(-0.5,-0.5,-0.5,-0.5,1000);
+    }
+
+    private void adjustScoreAngel(int bsPosition) {
+
+    }
+
+    private void adjustPitLocation(int pitLocation) {
+
+    }
+    private void scorePositioning() {
+        locateCryptoBoxCenter(balanceStonePosition);
+        adjustScoreAngel(balanceStonePosition);
+        adjustPitLocation(myPitLocation);
+    }
+
+    private void scoreGlyphs() {
+        imudrive(-90,0.5);
+        drivetime(0.5,0.5,0.5,0.5,500);
+        grabber.setPower(0.5);
+        sleep(1000);
+        grabber.setPower(-0.01);
+        drivetime(-0.5,-0.5,-0.5,-0.5,300);
     }
 }
