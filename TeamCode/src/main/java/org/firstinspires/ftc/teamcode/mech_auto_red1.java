fceb7b81921a404a7415e7b2a9a88ae945ebbacd
AndrewLuGit <andrew.yunxiang@gmail.com>
2017-12-11 09:06:18 -0800

    adding encode drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech_auto_red1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech_auto_red1.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
@@ -42,57 +43,90 @@ public class mech_auto_red1 extends LinearOpMode {
     private Orientation angles;
     private OpenGLMatrix lastLocation = null;
     private VuforiaLocalizer vuforia;
+    private BNO055IMU.AccelerationIntegrator myIntegrator;
     private boolean myTeamRed = true;
     private int myBSPosition = 4; /* 1: top lfts 2: top right 3: bottom lfts 4:bootom right */
     private int myPictoLocation = 0;    /* 1 : lfts 0: center -1 : right */
-    @Override
-    public void runOpMode() throws InterruptedException {
-        drivelf = hardwareMap.dcMotor.get("drivelf");
-        driverf = hardwareMap.dcMotor.get("driverf");
-        drivelb = hardwareMap.dcMotor.get("drivelb");
-        driverb = hardwareMap.dcMotor.get("driverb");
-        driverf.setDirection(DcMotor.Direction.REVERSE);
-        driverb.setDirection(DcMotor.Direction.REVERSE);
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
+
+    //------------------------------------------------------------------------------------------------
+    // Construction
+    //------------------------------------------------------------------------------------------------
+
+   public void initialize () {
+       BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+       VuforiaLocalizer.Parameters paramters2 = new VuforiaLocalizer.Parameters();
+
+       telemetry.addLine("get dirve");
+       drivelf = hardwareMap.dcMotor.get("drivelf");
+       driverf = hardwareMap.dcMotor.get("driverf");
+       drivelb = hardwareMap.dcMotor.get("drivelb");
+       driverb = hardwareMap.dcMotor.get("driverb");
+       driverf.setDirection(DcMotor.Direction.REVERSE);
+       driverb.setDirection(DcMotor.Direction.REVERSE);
+       myIntegrator = new FineAccelerationIntegrator();
+       parameters = new BNO055IMU.Parameters();
+       parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+       parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+       parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
+       parameters.loggingEnabled      = true;
+       parameters.loggingTag          = "IMU";
+       parameters.accelerationIntegrationAlgorithm = myIntegrator;
+       imu = hardwareMap.get(BNO055IMU.class,"imu");
+       imu.initialize(parameters);
+
+       telemetry.addLine("Init IMU");
         /*
         grabber = hardwareMap.crservo.get("grabber");
         grabberMotor = hardwareMap.dcMotor.get("grabberMotor");
         jewelHitter = hardwareMap.servo.get("jewelHitter");
         colorRange = (LynxI2cColorRangeSensor) hardwareMap.get("color");
         */
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
-        parameters.loggingEnabled      = true;
-        parameters.loggingTag          = "IMU";
-        parameters.accelerationIntegrationAlgorithm = new FineAccelerationIntegrator();
-        imu = hardwareMap.get(BNO055IMU.class,"imu");
-        imu.initialize(parameters);
-        imu.startAccelerationIntegration(new Position(), new Velocity(), 20);
-        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-        /*
-        VuforiaLocalizer.Parameters paramters2 = new VuforiaLocalizer.Parameters();
         paramters2.vuforiaLicenseKey = "AVpbLJb/////AAAAGXZuk17KREdul0PqldXjI4ETC+yUOY/0Kn2QZcusavTR02WKxGvyI4E5oodS5Jta30WYJtnJuH7AhLaMe8grr9UC2U3qlnQkypIAZsR8xa38f669mVIo9wujvkZpHzvscPZGdZ2NaheUepxU/asMbuldnDOo3TjSYiiEbk1N3OkxdTeMa4W+BOyrO6sD8L7bcPfnFpmuOPRv0+NeEUL638AjNyi+GQeHYaSLsu6u4ONKtwF+axjjg0W+LRgp5T/5oWxexW3fgoMrkijzsJ0I5OuxSdCeZ3myJthxcyHwHqdhuxmWFvFOoYgJ4k6LdGNijymNWqMp97utjg8YXMAguMLJU2QkPJvZQqbkzIdjzzQk";
         paramters2.cameraDirection = VuforiaLocalizer.CameraDirection.FRONT;
-        this.vuforia = ClassFactory.createVuforiaLocalizer(paramters2);
+        vuforia = ClassFactory.createVuforiaLocalizer(paramters2);
+
+       telemetry.addLine("Init Vuforia");
+        /*
         VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
         VuforiaTrackable relicTemplate = relicTrackables.get(0);
         */
         /*
-        jewelHitter.setPosition(0.05);
-        */
-        telemetry.addLine("Init Ready");
-        telemetry.update();
-        sleep(100);
+       drivelf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+       driverf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+       drivelb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+       driverb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+       */
+       drivelf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+       driverf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+       drivelf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+       driverf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+       telemetry.addLine("reset encode");
+       telemetry.addLine("Init Ready");
+       telemetry.update();
+   }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
         telemetry.clear();
+        initialize();
         /* start of the code */
         waitForStart();
         /*
+         jewelHitter.setPosition(0.05);
+        */
+        /*
         relicTrackables.activate();
         */
-
-
-        // Clear telemetry
 
 
         /*
@@ -112,7 +146,10 @@ public class mech_auto_red1 extends LinearOpMode {
         sleep(500);
         kickOpponentJewel(myTeamRed);
  */
-        test_imudrive();
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 20);
+        //test_imudrive();
+        test_encode();
+        //drivetime(0.6,0.6,-0.6,-0.6,1000);
         /* get my pit location by scan the Vulmark */
         /* get to the right postion before unload Glyphs */
      //   scorePositioning();
@@ -121,16 +158,93 @@ public class mech_auto_red1 extends LinearOpMode {
         imu.stopAccelerationIntegration();
     }
 
+
+    private  void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+
+        ElapsedTime runtime = new ElapsedTime();
+
+        drivelf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        driverf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        drivelf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        driverf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Determine new target position, and pass to motor controller
+        newLeftTarget = drivelf.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+        newRightTarget = driverf.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+
+        drivelf.setTargetPosition(newLeftTarget);
+        driverf.setTargetPosition(newRightTarget);
+
+        newLeftTarget = drivelb.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+        newRightTarget = driverb.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+
+        drivelb.setTargetPosition(newLeftTarget);
+        driverb.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+        drivelf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        driverf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        drivelb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        driverb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+        runtime.reset();
+        drivelf.setPower(Math.abs(speed));
+        driverf.setPower(Math.abs(speed));
+        drivelb.setPower(Math.abs(speed));
+        driverb.setPower(Math.abs(speed));
+
+
+        // keep looping while we are still active, and there is time left, and both motors are running.
+        // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+        // its target position, the motion will stop.  This is "safer" in the event that the robot will
+        // always end the motion as soon as possible.
+        // However, if you require that BOTH motors have finished their moves before the robot continues
+        // onto the next step, use (isBusy() || isBusy()) in the loop test.
+
+        while ((runtime.seconds() < timeoutS) &&
+                (drivelf.isBusy() && driverf.isBusy())) {
+
+            // Display it for the driver.
+            telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+            telemetry.addData("Path2",  "Running at %7d :%7d",
+                    drivelf.getCurrentPosition(),
+                    driverf.getCurrentPosition());
+            telemetry.update();
+        }
+
+        // Stop all motion;
+        drivelf.setPower(0);
+        driverf.setPower(0);
+        drivelb.setPower(0);
+        driverb.setPower(0);
+            // Turn off RUN_TO_POSITION
+        drivelf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driverf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        drivelb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driverb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    private void test_encode() {
+        encoderDrive(DRIVE_SPEED,  24,  24, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+       // encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+       // encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+    }
+
     /* The following function is to revert normalize happening in library,  it is not desired
-     * revert here.  here is the normalize function
-     * static double normalizeDegrees(double degrees)
-     * {
-     *  while (degrees >= 180.0) degrees -= 360.0;
-     * while (degrees < -180.0) degrees += 360.0;
-     * return degrees;
-     *  }
-     *
-     */
+        * revert here.  here is the normalize function
+        * static double normalizeDegrees(double degrees)
+        * {
+        *  while (degrees >= 180.0) degrees -= 360.0;
+        * while (degrees < -180.0) degrees += 360.0;
+        * return degrees;
+        *  }
+        *
+        */
     private double revert_normalize(double initDegrees, double currDegrees) {
         if (initDegrees > 0 && currDegrees < 0  ) {
             currDegrees += 360;
@@ -139,7 +253,6 @@ public class mech_auto_red1 extends LinearOpMode {
         }
         return  currDegrees;
     }
-
 
     private void imudrive(double turnDegrees,double k1){
         angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
@@ -183,6 +296,10 @@ public class mech_auto_red1 extends LinearOpMode {
     }
 
     private void drivetime(double lfPower,double rfPower, double lbPower, double rbPower,long milliseconds){
+        drivelf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        driverf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        drivelf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        driverf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         drivelf.setPower(lfPower);
         driverf.setPower(rfPower);
         drivelb.setPower(lbPower);
@@ -219,6 +336,27 @@ public class mech_auto_red1 extends LinearOpMode {
         imudrive(-120, 9.0);
         sleep(1000);
         imudrive(-100, 9.0);
+
+    }
+
+    private void moveto(Position destination) {
+        Position current = imu.getPosition();
+        double x_offset = destination.x - current.x;
+        double y_offset = destination.y - current.y;
+        double turn_degree = Math.atan2(y_offset, x_offset);
+        double distance =   Math.sqrt(x_offset * x_offset + y_offset * y_offset);
+        long drive_time = (long) distance / 100;
+
+        while (drive_time > 1) {
+            imudrive(turn_degree, 9.0);
+            drivetime(1.0, 1.0, 1.0, 1.0, drive_time);
+            current = imu.getPosition();
+            x_offset = destination.x - current.x;
+            y_offset = destination.y - current.y;
+            turn_degree = Math.atan2(y_offset, x_offset);
+            distance = Math.sqrt(x_offset * x_offset + y_offset * y_offset);
+            drive_time = (long) distance / 100;
+        }
 
     }
 
